117e39fa546c178d9b5152946cfaed4f0e7a13a5
Svancik <thesvancik@gmail.com>
2022-11-13 19:29:16 +0100

    embdaCarousel

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import EmblaCarousel from "../../components/emblaCarousel/EmblaCarousel";
 import SideMenu from "../../components/sideMenu/SideMenu";
 import Topbar from "./../../components/topbar/Topbar";
 import "./home.css";
@@ -11,8 +12,19 @@ export default function Home() {
           <div className="sideMenu">
             <SideMenu />
           </div>
-          <div className="horizontalCarousel"></div>
-          <div className="library"></div>
+          <div className="contentWrapper">
+            <div className="horizontalCarousel">
+              {/* <EmblaCarousel autoplay delayLength={2000}>
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+              </EmblaCarousel> */}
+            </div>
+
+            <div className="library"></div>
+          </div>
         </div>
       </div>
     </div>
